3203612f478ec01573b6f0d4d56ec7cfe8b5623c
Bruno Gabriel <brunospagi@gmail.com>
2025-05-10 19:56:56 -0300

    resolvendo url admin para login

--- sept/urls.py
+++ sept/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from core import views
 
 urlpatterns = [
-    path('/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('', views.dashboard, name='dashboard'),
     path('importar/', views.importar_csv, name='importar_csv'),
     path('scan/', views.scan_codigo, name='scan'),
